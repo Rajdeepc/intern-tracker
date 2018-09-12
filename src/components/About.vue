12b398c12184bf3c1c0554383a4ba636bfb831ba
Rajdeepc <rajrock38@gmail.com>
2018-09-13 01:07:20 +0530

    First commit

---
+++ src/components/About.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="about"> 
+        <p>Hello {{ their_name }},This is about page</p>
+        
+    </div>
+</template>
+
+
+<script>
+export default {
+  name: "Skills",
+  data() {
+    return {
+     their_name: this.$route.params.name
+    }
+  }
+};
+</script>
