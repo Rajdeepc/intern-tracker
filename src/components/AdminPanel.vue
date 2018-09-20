0a301abbb8151c9e0702627936080fa6322af90a
Chandra, Rajdeep <rajdeep.chandra@accenture.com>
2018-09-20 20:54:56 +0530

    new input

--- src/components/AdminPanel.vue
+++ src/components/AdminPanel.vue
@@ -1,22 +1,20 @@
 <template>
   <div>
-    <b-modal id="modallg" title="Add Project Details" @ok="handleOk">
-      <form @submit.stop.prevent="handleSubmit">
+    <b-modal id="modallg" title="Add Project Details">
+      <form @submit.prevent action="/addproject" method="post">
         <p>This panel is only visible to TL and Above</p>
         <div class="form-group">
           <label for="exampleInputEmail1">Your Name</label>
           <input type="text" class="form-control" id="" v-model="getUsername" readonly="" name="ownedBy" aria-describedby="emailHelp">
         </div>
-                <input type="hidden" v-model="no_of_members"/>
-
-        <div class="form-group">
+          <div class="form-group">
           <label for="exampleInputEmail1">Add Your Project</label>
           <input type="text" class="form-control" id="" v-model="project_name" readonly="" name="ownedBy" aria-describedby="emailHelp">
         </div>
-        <label for="exampleInputEmail1">Add Members</label>
         <button class="btn btn-primary float-right" @click="addFields()">Add More</button>
         <div class="form-group" v-for="(newInput,index) in newInputArray" :key='index'>
           <input type='text' class='form-control' id='' aria-describedby='emailHelp' placeholder='Enter member email id' v-model="memberArr[index]">
+          <a v-on:click="removeElement(index);" style="cursor: pointer">Remove</a>
         </div>
   
   </form>
@@ -25,41 +23,23 @@
 </template>
 
 <script>
-  import DataPostApi from "../services/api/loginValidation";
-
   export default {
     name: "AdminPanel",
     props: ["getUsername"],
     data() {
       return {
         newInputArray:[""],
-        memberArr: [],
-        project_name:'',
-        no_of_members:this.newInputArray.length
+        memberArr: []
       };
     },
     mounted() {},
     methods: {
       addFields: function() {
         this.newInputArray.push("");
-        console.log(this.newInputArray);
       },
-      handleOk: function(e){
-        e.preventDefault();
-        if(!this.project_name){
-          alert('Please enter the project name');
-        } else {
-          this.handleSubmit();
-        }
+      removeElement: function(index) {
+       this.newInputArray.splice(index, 1);
       },
-      handleSubmit: function(){
-        DataPostApi.projectsaveApi(date_created,ownedBy,project_name,no_of_members,member_names)
-        .then(response => {
-            if(response.status === "saved") {
-              console.log('Data saved to db');
-            }
-        })
-      }
     }
   };
 </script>
