7472efc47814f121a40e6d717f63dfd4f49b9a14
Chandra, Rajdeep <rajdeep.chandra@accenture.com>
2018-09-20 20:27:34 +0530

    new input

--- src/components/AdminPanel.vue
+++ src/components/AdminPanel.vue
@@ -1,18 +1,20 @@
 <template>
   <div>
-    <b-modal id="modallg" title="Add Project Details">
-      <form @submit.prevent action="/addproject" method="post">
+    <b-modal id="modallg" title="Add Project Details" @ok="handleOk">
+      <form @submit.stop.prevent="handleSubmit">
         <p>This panel is only visible to TL and Above</p>
         <div class="form-group">
           <label for="exampleInputEmail1">Your Name</label>
           <input type="text" class="form-control" id="" v-model="getUsername" readonly="" name="ownedBy" aria-describedby="emailHelp">
         </div>
-          <div class="form-group">
+                <input type="hidden" v-model="no_of_members"/>
+
+        <div class="form-group">
           <label for="exampleInputEmail1">Add Your Project</label>
           <input type="text" class="form-control" id="" v-model="project_name" readonly="" name="ownedBy" aria-describedby="emailHelp">
         </div>
-                  <label for="exampleInputEmail1">Add Members</label>
-<button class="btn btn-primary float-right" @click="addFields()">Add More</button>
+        <label for="exampleInputEmail1">Add Members</label>
+        <button class="btn btn-primary float-right" @click="addFields()">Add More</button>
         <div class="form-group" v-for="(newInput,index) in newInputArray" :key='index'>
           <input type='text' class='form-control' id='' aria-describedby='emailHelp' placeholder='Enter member email id' v-model="memberArr[index]">
         </div>
@@ -23,6 +25,8 @@
 </template>
 
 <script>
+  import DataPostApi from "../services/api/loginValidation";
+
   export default {
     name: "AdminPanel",
     props: ["getUsername"],
@@ -30,14 +34,31 @@
       return {
         newInputArray:[""],
         memberArr: [],
-        project_name:''
+        project_name:'',
+        no_of_members:this.newInputArray.length
       };
     },
     mounted() {},
     methods: {
       addFields: function() {
         this.newInputArray.push("");
-  
+        console.log(this.newInputArray);
+      },
+      handleOk: function(e){
+        e.preventDefault();
+        if(!this.project_name){
+          alert('Please enter the project name');
+        } else {
+          this.handleSubmit();
+        }
+      },
+      handleSubmit: function(){
+        DataPostApi.projectsaveApi(date_created,ownedBy,project_name,no_of_members,member_names)
+        .then(response => {
+            if(response.status === "saved") {
+              console.log('Data saved to db');
+            }
+        })
       }
     }
   };
