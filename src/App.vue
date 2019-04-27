02a06b599150928eb0def4b1ace815abc39624de
Rajdeepc <rajrock38@gmail.com>
2019-04-27 18:17:46 +0530

    new commit

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,82 @@
 <template>
   <div id="app">
-      <router-view></router-view>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-// import Skills from './components/Skills.vue'
-import Login from './components/Login.vue'
-
-export default {
-  name: 'app',
-  components: {
-    Login
+  // import Skills from './components/Skills.vue'
+  import Login from './components/Login.vue'
+  
+  export default {
+    name: 'app',
+    components: {
+      Login
+    }
   }
-}
 </script>
 
 <style>
-@import url('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css');
-@import url('https://fonts.googleapis.com/css?family=Montserrat:400,700');
-body {
-  background-color: #EEEEEE;
-  font-family: 'Montserrat', sans-serif;
-  display: grid;
-  grid-template-rows: auto;
-  justify-items: center;
-}
-body, html {
-  margin: 0;
-  height: 100%;
-}
-#app {
-    width: 90%;
-}
-nav {
-  padding: 20px 20px 20px 0;
-}
-nav a {
-  padding: 10px;
-  text-decoration: none;
-  background: #fff;
-  border-radius: 3px;
-  color: rgb(0, 110, 255);
-  font-weight: bold;
-  margin-right: 15px;
-}
+  @import url('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css');
+  @import url('https://fonts.googleapis.com/css?family=Montserrat:400,700');
+  body {
+    background-color: #EEEEEE;
+    display: grid;
+    grid-template-rows: auto;
+    justify-items: center;
+    font-family: 'Roboto', sans-serif !important;
+  }
+  
+  body,
+  html {
+    margin: 0;
+    height: 100%;
+  }
+  
+  #app {
+    width: 100%;
+  }
+  
+  .bodywrapper {
+    padding: 3em 2em;
+    background: #fafafa;
+  }
+  
+  nav {
+    padding: 20px 20px 20px 0;
+  }
+  
+  nav a {
+    padding: 10px;
+    text-decoration: none;
+    background: #fff;
+    border-radius: 3px;
+    color: rgb(0, 110, 255);
+    font-weight: bold;
+    margin-right: 15px;
+  }
+    .width3{
+        width: 3%
+    }
+    .width40{
+        width: 40%
+    }
+    .width10{
+        width: 10%
+    }
+    .width15{
+        width:15%
+    }
+    .width30{
+        width:30%
+    }
+    .width7{
+        width:7%
+    }
+    .width15{
+        width:15%
+    }
+    .width25{
+        width:25%
+    }
 </style>
---
+++ src/utils/constants.js
@@ -0,0 +1,5 @@
+const ADMIN_ARRAY = ['rajdeep.chandra@accenture.com','n.balanarasimhalu@accenture.com','prem.parthasarathy@accenture.com']
+
+export default {
+    ADMIN_ARRAY:ADMIN_ARRAY
+}
